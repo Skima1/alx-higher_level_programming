916eda6ef2ac198198fe16198cbaa488eacb4d1e
Oloniyo Sunday <ayodeleoloniyo@gmail.com>
2023-03-27 12:57:42 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
+    n = 0
+    for m in range(x):
         try:
-            print(my_list[i], end='')
+            print(my_list[m], end="")
+            n += 1
         except IndexError:
-            print()
-            return i
+            break
     print()
-    return x
+    return n
