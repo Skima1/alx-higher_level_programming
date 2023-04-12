4ef0ba9153a62a2ce94c061a82d2b9daef93927d
Oloniyo Sunday <ayodeleoloniyo@gmail.com>
2023-04-12 20:01:19 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+"""Create a UTF-8 text file and write a string to it"""
 
-"""
-
-Contains the function "wrtie_file"
-
-"""
 
 def write_file(filename="", text=""):
-
-    """returns the number of chars written to "filename" from "text" """
-
-    with open(filename, 'w', encoding='utf=8') as f:
-
+    """Create a text file and write a string to it.
+    Args:
+    filename (str): The name of the file to create.
+    text (str): The text to write to the file.
+    Returns:
+    The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
