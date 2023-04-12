95fa13e5ebd828a2b9206293d58f50a15e148f73
Oloniyo Sunday <ayodeleoloniyo@gmail.com>
2023-04-12 20:10:06 +0100

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class Student."""
 
-"""
-
-Contains the clas "Student"
-
-"""
 
 class Student:
-
-    """Representation of a student"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-
-        """Initializes the student"""
-
+        """Initialize a new Student object
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
-
         self.last_name = last_name
-
         self.age = age
 
     def to_json(self):
-
-        """returns a dictionary representation of a Student instance"""
-
-        return self.__dict__ 
+        """Get a dict representation of the Student."""
+        return self.__dict__
